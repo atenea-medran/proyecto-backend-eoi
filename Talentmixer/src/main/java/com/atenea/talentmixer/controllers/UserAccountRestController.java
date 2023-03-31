8a02f8f5e85c1d5077f4c47d78fe1efd259a9e8d
ateneamedran <ateneamedran@protonmail.com>
2023-03-31 12:53:14 +0200

    1253

--- Talentmixer/src/main/java/com/atenea/talentmixer/controllers/UserAccountRestController.java
+++ Talentmixer/src/main/java/com/atenea/talentmixer/controllers/UserAccountRestController.java
@@ -19,9 +19,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.atenea.talentmixer.models.entities.Project;
 import com.atenea.talentmixer.models.entities.UserAccount;
 import com.atenea.talentmixer.models.services.IuserAccountService;
 
